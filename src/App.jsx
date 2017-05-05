04ea3ed68a12ee94383bf9e249b3eeb92c1dfb38
Tom Zmyslo <tzmyslo25@gmail.com>
2017-05-05 14:01:18 -0400

    Stubbed out more React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,77 @@
-// var contentNode = document.getElementById('contents')
-// var component = <h1>Hello Space!</h1>
-// ReactDOM.render(component, contentNode)
-
 const contentNode = document.getElementById('contents')
 
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe']
-const message = continents.map(c => `Hello ${c}!`).join(' ')
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Filter.</div>
+    )
+  }
+}
 
-const component = <p>{message}</p>
-ReactDOM.render(component, contentNode)
+class IssueRow extends React.Component {
+  static get propTypes() {
+    issue_id: React.PropTypes.number.isRequired,
+    issue_title: React.PropTypes.string
+  }
+
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 4}
+    return (
+      <tr>
+        <td style={borderedStyle}>{this.props.issue_id}</td>
+        <td style={borderedStyle}>{this.props.issue_title}</td>
+      </tr>
+    )
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 6}
+    return (
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={borderedStyle}>ID</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow
+            issue_id={1}
+            issue_title="Error in console when clicking Add"
+          />
+          <IssueRow
+            issue_id={2}
+            issue_title="Missing bottom border on panel"
+          />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Add entry form.</div>
+    )
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode)
